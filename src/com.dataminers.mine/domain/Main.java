476e513a5580f09fc79bf4b31418e43e31a77659
ktzouva <kostastzouva@gmail.com>
2019-05-19 01:15:48 +0300

    SQLInjections

--- src/com.dataminers.mine/domain/Main.java
+++ src/com.dataminers.mine/domain/Main.java
@@ -1,6 +1,7 @@
 package domain;
 import database.DatabaseConnection;
 import database.DatabaseData;
+import regex.RegexMatches;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -10,11 +11,12 @@ public class Main {
     {
         DatabaseConnection dc = new DatabaseConnection();
         Connection connection = dc.connectDatabase();
-
         DatabaseData dbdata = new DatabaseData();
+        RegexMatches rm = new RegexMatches();
         try {
             System.out.println("Incoming Server Traffic: "+dbdata.getIncomingServerTraffic(connection)+"\n Sum of 500 errors: "+
                     dbdata.getServerErrorRequests(connection) + "\n Unique ips: " + dbdata.getUniqueIps(connection) );
+            System.out.println("Number of SQL Injections : "+rm.numberOfSQLinjection(dbdata.getRequests(connection)));
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();

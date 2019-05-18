e992b5beeaffc218f5fca63cf90fd52be108f316
Anestis <anestis.zioulis@outlook.com>
2019-05-18 19:36:40 +0300

    Added General queries

--- src/com.dataminers.mine/database/DatabaseData.java
+++ src/com.dataminers.mine/database/DatabaseData.java
@@ -1,22 +1,56 @@
 package database;
 
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseData {
+    private Statement statement = null;
+    private ResultSet result;
 
     public ResultSet getRequests(Connection connection){
-        Statement statement = null;
         try {
             statement = connection.createStatement();
-            ResultSet result = statement.executeQuery("SELECT request, ip FROM logs");
+            result = statement.executeQuery("SELECT request, ip FROM logs");
             return result;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
     }
+    public ResultSet getIncomingServerTraffic (Connection connection) {
+        try {
+            statement = connection.createStatement();
+            result = statement.executeQuery( "SELECT * FROM `logs` WHERE requests LIKE 'GET%'");
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public ResultSet getServerErrorRequests (Connection connection) {
+        try {
+            statement = connection.createStatement();
+            result = statement.executeQuery( "SELECT COUNT (requests) AS SUM FROM ``logs" +
+                    "WHERE requests LIKE 'GET%'");
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public ResultSet getUniqueIps (Connection connection) {
+        try {
+            statement = connection.createStatement();
+            result = statement.executeQuery( "SELECT COUNT(ip) as sum FROM (SELECT ip FROM `logs` GROUP BY ip) as ip");
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
